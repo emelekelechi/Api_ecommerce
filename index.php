0eede0891158942b5a516e03fc2ca1a6f742f2c0
kelechi <emelekelex@gmail.com>
2020-11-25 07:24:06 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<meta name="viewport"
+    content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" type="text/css" href="index.css" />
+
+
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+<meta name="description"
+    content="Official portfolio website of Elite Samuel" />
+<title>Kelex Emele</title>
+</head>
+<body id="body">
+    <header class="header" id="home">
+        <div id="background">
+            <nav id="nav">
+                <div id="nav-logo"> 
+                    <a href="#">EliteSam</a>
+                </div>
+                <div id="nav-text">
+                    <a href="#about">ABOUT</a>
+                    <a href=" #resume">RESUME</a>
+                    <a href="#portfolio">PORTFOLIO</a>
+                    <a href="#contact">CONTACT</a>
+                </div>
+                <span class="icon" onclick="opensidenav()">&#9776;</span>
+                    <div id="my_sidenav">
+                        <span class="icon1" onclick="closenav()">&times;</span><br>
+                        <div class="dropdown-divider"></div>
+                        <a id="a" href="#home" class="links">HOME</a>
+                        <div class="dropdown-divider"></div>
+                        <a id="a" href="#about" class="links">ABOUT</a>
+                        <div class="dropdown-divider"></div>
+                        <a id="a" href="#resume" class="links">RESUME</a>
+                        <div  class="dropdown-divider"></div>
+                        <a id="a" href="#portfolio" class="links">PORTFOLIO</a>
+                        <div class="dropdown-divider"></div>
+                        <a id="a" href="#contact" class="links">CONTACT</a>
+                        <div class="dropdown-divider"></div>
+                    </div>
+            </nav>
+            <div id="header_img_container">
+                <img alt="" id="header_img" src="kelex3134.png">
+                <div id="header_img_container_overlay">
+                    <p id="welcome">Hello everyone,</p>
+                    <p id="welcome_two">I'm 
+                    <br>Emele Samuel</p>
+                    <p id="welcome_three">A web Developer &amp; Tutor</p>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+    <div class="container" style="margin-top:50px">
+        <h1 style="color:orangered; text-align:center" id="about">About</h1>
+        <h3 style="text-align:center">About Me</h3>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-5">
+                <img src="kel4.jpg" alt="nysc" width="100%" height="400px"> 
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-7" id="padding" style="margin-top:50px">
+                <p>I'm a fullstack web developer with extensive knowledge and experience in building webs application using HTML, CSS, Javascript, Bootstrap, PHP and mySQL. I Love to create solutions that come alive and solve critical problems. I'm driven, industrious and love human interaction. You 'll find me staking out a challenge, fleshing out a problem or just chilling at a meeetup with friends and family. I also love sharing knowledge, that why I freelance as a part-time tutor and writer, teaching and writing about web development concepts. I design and develop web applications for desktop, mobile(android&ios) and web platform; and have been working in the field for more than 2 years. I would Love to help you take your product to the next level.</p>
+            </div>
+        </div>
+    </div>
+    <div class="dropdown-divider"></div>
+    
+    <div class="container" style="margin-top:50px">
+        <h1 style="color:orangered; text-align:center" id="resume">Resume</h1>
+        <h1 style="color:orangered; text-align:center">My Resume</h1>
+        <h3 style="text-align: center; margin-bottom:30px">EDUCATION</h3>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-4">
+                <div id="padding2">
+                    <span>Degree of Banking and Finance, Nnamdi Azikiwe University</span><br>
+                    <span>4 Years Course</span><br>
+                    <span>DONE</span><br>
+                    <span>GRADUATED AUGUST 2018</span>
+                    <div class="dropdown-divider"></div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4">
+                <div id="padding2">
+                    <span>Transparency Information and communication Technology Academy</span><br>
+                    <span>3 Months</span><br>
+                    <span>DONE</span><br>
+                    <span>GRADUATED OCTOBER 2018</span>
+                    <div class="dropdown-divider"></div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4">
+                <div id="padding2">
+                    <span>DveryBest Android Application and Web Development Academy</span><br>
+                    <span>6 months</span><br>
+                    <span>DONE</span><br>
+                    <span>GRADUATED JULY 2020</span>
+                    <div class="dropdown-divider"></div>
+                </div>
+            </div>
+        </div>
+        <h3 style="text-align: center; margin-bottom:30px">WORK</h3>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6">
+                <div id="padding2">
+                    <span>Remote Web Application Developer and tutor</span><br>
+                    <span>Transparency Information and communication Technology Academy</span><br>
+                    <span>JANUARY 2019 – AUGUST 2020</span><br><br>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6">
+                <div id="padding2">
+                    <span>Freelance Web Developer and Search Engine Optimization Expert</span><br>
+                    <span>No. 25 Okpara Avenue, GRA, Enugu, Enugu state, Nigeria</span><br>
+                    <span>APRIL 2020 - Till Date</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="dropdown-divider"></div><br><br><br>
+    <h1 style="color:orangered; text-align:center" id="portfolio"  style="margin-top:50px">My Portfolio</h1>
+    <div class="container-fluid">
+        <div class="row px-0 mx-0 r2">
+            <div class="col-lg-4 px-0">
+                <div class="card" style="margin-bottom:50px; margin-right:5px">
+                    <img class="card-img-top" src="life.PNG" height="300" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-text" style="text-align:center; color:gray"><b>De Matteu skill acquisition center<br> The best skills acquisition center</b></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 px-0">
+                <div class="card" style="margin-bottom:50px; margin-right:5px">
+                    <img class="card-img-top" src="life3.PNG" height="300" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-text" style="text-align:center; color:gray"><b>The Healthy Life<br>Secret of Healthy Living</b></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 px-0">
+                <div class="card" style="margin-right:5px; margin-bottom:50px">
+                    <img class="card-img-top" src="life5.PNG" height="300" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-text" style="text-align:center; color:gray"><b>EliteSam ICT Academy<br>We are ruling  the tech world</b></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="row px-0 mx-0 r2">
+            <div class="col-lg-4 px-0 offset-lg-2">
+                <div class="card" style="margin-bottom:50px; margin-right:5px">
+                    <img class="card-img-top" src="life2.PNG" height="300" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-text" style="text-align:center; color:gray"><b>Kelex International Business Outlet<br> We are world class stylist</b></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 px-0 offst-lg-2">
+                <div class="card" style="margin-bottom:50px; margin-right:5px">
+                    <img class="card-img-top" src="life6.PNG" height="300" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-text" style="text-align:center; color:gray"><b>Kelex International Fashion Moore<br> We sell all fashion wear of kinds</b></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="dropdown-divider"></div>
+    <div class="contact container px-0" id="contact" style="margin-top:50px; margin-bottom:100px">
+        <h2 style="color:orangered; text-align:center" id="contact">CONTACT</h2>
+        <h3 style="text-align:center">Let's work together</h3>
+        <div class="contact-links">
+            <a href="https://web.facebook.com/kelex.emele" target="_blank" class="btn contact-details"><i class="fab fa-facebook"></i> Facebook</a>
+            <a id="profile-link" href="https://github.com/emelekelechi" target="_blank" class="btn contact-details"><i class="fab fa-github"></i> GitHub</a>
+            <a href="https://twitter.com/EliteSamuel3" target="_blank" class="btn contact-details"><i class="fab fa-twitter"></i> Twitter</a>
+            <a href="mailto:emelekelex@gmail.com" class="btn contact-details"><i class="fa fa-at"></i> Send a mail</a>
+            <a href="tel:+2348166761792" class="btn contact-details"><i class="fa fa-mobile"></i> Call me</a>
+        </div>
+    </div>
+    <div class="dropdown-divider"></div>
+        <footer>
+            <span id="date">Copyright ©2020 Kelex International. All Rights Reserved. Powered by Elite Sam</span>
+        </footer>
+    
+    
+
+</body>
+</html>
+
+<script>
+        function opensidenav(){
+            document.getElementById("my_sidenav").style.width ="260px";
+        }
+            function closenav(){
+                document.getElementById("my_sidenav").style.width ="0%";
+            }
+            
+</script>
+<script>
+    function myFunction(){
+        var top = document.getElementById("nav");
+        var ypos = window.pageYOffset;
+        if(ypos > 170){
+            top.classList.add("display");
+        }
+        else{
+            top.classList.remove("display");
+        }
+    }
+    window.addEventListener("scroll", myFunction);
+</script>
